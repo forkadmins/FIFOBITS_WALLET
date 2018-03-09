17d48d38da3db43b4992e3347cdeea667b264774
forkadmins <admin@fifobits.com>
2018-03-09 20:04:30 +0530

    first commit

---
+++ CMakeFiles/fifobits.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fifobits/cryptonotewallet/build/fifobits_automoc.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/fifobits_automoc.cpp.o"
+  "/home/fifobits/cryptonotewallet/build/qrc_resources.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/qrc_resources.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/CommandLineParser.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/CommandLineParser.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/CryptoNoteWrapper.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/CryptoNoteWrapper.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/CurrencyAdapter.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/CurrencyAdapter.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/LoggerAdapter.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/LoggerAdapter.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/NodeAdapter.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/NodeAdapter.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/Settings.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/Settings.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/SignalHandler.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/SignalHandler.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/WalletAdapter.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/WalletAdapter.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/AboutDialog.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/AboutDialog.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/AddressBookDialog.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/AddressBookDialog.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/AddressBookFrame.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/AddressBookFrame.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/AddressBookModel.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/AddressBookModel.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/AnimatedLabel.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/AnimatedLabel.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/ChangePasswordDialog.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/ChangePasswordDialog.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/ExitWidget.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/ExitWidget.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/MainWindow.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/MainWindow.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/NewAddressDialog.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/NewAddressDialog.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/NewPasswordDialog.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/NewPasswordDialog.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/OverviewFrame.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/OverviewFrame.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/PasswordDialog.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/PasswordDialog.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/ReceiveFrame.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/ReceiveFrame.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/RecentTransactionsModel.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/RecentTransactionsModel.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/SendFrame.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/SendFrame.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/SortedTransactionsModel.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/SortedTransactionsModel.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/TransactionDetailsDialog.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/TransactionDetailsDialog.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/TransactionFrame.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/TransactionFrame.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/TransactionsFrame.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/TransactionsFrame.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/TransactionsListModel.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/TransactionsListModel.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/TransactionsModel.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/TransactionsModel.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/gui/TransferFrame.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/gui/TransferFrame.cpp.o"
+  "/home/fifobits/cryptonotewallet/src/main.cpp" "/home/fifobits/cryptonotewallet/build/CMakeFiles/fifobits.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GIT_REVISION=\"a42d92e\""
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "_GNU_SOURCE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/fifobits/cryptonotewallet/build/CMakeFiles/cryptonote.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../src"
+  "../cryptonote/external"
+  "../cryptonote/include"
+  "../cryptonote/src"
+  "../cryptonote/src/Platform/Linux"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/fifobits_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "cryptonote_automoc.cpp"
+  "fifobits_automoc.cpp"
+  "CMakeFiles/fifobits_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/fifobits_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
